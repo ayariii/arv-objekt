336577b15ab416eec5d7fd6ba74239dc65185902
ayariii <90261694+ayariii@users.noreply.github.com>
2022-10-04 11:26:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program 
+{
+    public static void Main() 
+    {
+        Console.WriteLine("Type width: ");
+        int width = int.Parse(Console.ReadLine());
+        Console.WriteLine("Type height: ");
+        int height = int.Parse(Console.ReadLine());
+
+        Triangle t = new Triangle (width, height);
+        Rectangle r = new Rectangle (width, height);
+
+        Console.WriteLine("Rectangle has an area of " + r.Area() + "m");
+    }
+}
